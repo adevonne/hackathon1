2ec10f5332834bfd70a3c8d1ad4df3f601d14ffd
Ribeiro Jessica <ribeirofj@yahoo.com>
2023-05-12 12:06:41 +0200

    mise en forme de la page hotel

--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -19,7 +19,7 @@ export default function Results({ results }) {
           }}
           key={result.geoCode.latitude}
         >
-          <div className="card ">
+          <div>
             <p>{result.name}</p>
             <p>{result.address.countryCode}</p>
           </div>
