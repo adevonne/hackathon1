c5fe8d76108c85bad6cebcc57d4c985bc6de3aa9
Bertrand Robert <brobert.bertrand@wanadoo.fr>
2023-05-12 12:21:57 +0200

    okok

--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -3,13 +3,17 @@ import PropTypes from "prop-types";
 import hotelsimg from "../data/hotelsimg";
 
 export default function List({ hotels }) {
+  const randomImg = () => {
+    return hotelsimg[Math.floor(Math.random() * 10)].img;
+  };
+
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 text-center">
       {hotels.map((hotel) => (
         <div className="border-2 border-gray-950">
           <Link to={`/hotels/${hotel.hotelId}`}>
             <h2>{hotel.name}</h2>
-            <img src={hotelsimg[0].img} alt="hotel" className="" />
+            <img src={randomImg()} alt="hotel" className="" />
             <p>
               {hotel.distance.value}
               {hotel.distance.unit}
